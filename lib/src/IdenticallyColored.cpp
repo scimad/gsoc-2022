3ca58b0c766337d90ba687fea7acf3e7bc6baba6
Madhav Humagain <scimad2.71@gmail.com>
2022-03-28 16:48:09 +0545

    Complete connected component | Add result print functionality

--- lib/src/IdenticallyColored.cpp
+++ lib/src/IdenticallyColored.cpp
@@ -27,16 +27,26 @@ namespace gsoc
             unvisited_vertices.emplace(i);
         }
         std::vector<Eigen::Vector3d> colors;
-        std::set<std::set<int>> iccps;   //identically connected colored components [[The] [final] [result]]
+
+        // iccps: identically connected colored components [[The] [final] [result]]
+        // this can be vector because vidx guarantes that the order of smallest element is ascending
+        // std::vector<std::set<int>> iccps;
+
+        std::vector<std::set<int>> connected_components;
+
         while (!unvisited_vertices.empty())
-        {   
+        {
             int vidx = *(unvisited_vertices.begin());
             std::cout << "Checking for vert " << vidx << std::endl;
             if (unvisited_vertices.count(vidx))
             {
                 std::queue<int> connected_queue_dfs;
                 connected_queue_dfs.push(vidx);
+
+                // connected_component: represents one connected component (subgraph) of graph
+                ///To maintain ascending index, we store vidx in set
                 std::set<int> connected_component;
+
                 while (!connected_queue_dfs.empty())
                 {
                     int vert = connected_queue_dfs.front();
@@ -46,7 +56,6 @@ namespace gsoc
                         continue;
                     }
                     unvisited_vertices.erase(vert);
-                    std::cout << " Now I have visited vert " << vert << "." << std::endl;
                     auto adj_l = mesh.adjacency_list_[vert];
                     for (auto adj_v : adj_l){
                         if (unvisited_vertices.count(adj_v)){
@@ -54,8 +63,16 @@ namespace gsoc
                         }
                     }
                 }
-                std::cout << "The new connected component has " << connected_component.size() <<"vertices."<< std::endl;
+                connected_components.insert(connected_components.end(), connected_component);
             }
+        } //Completed visiting all vertices
+        std::cout << "Printing result: "<< std::endl;
+        for (auto cc : connected_components){
+            std::cout << "Using ierator" << std::endl;
+            for (auto it = cc.begin(); it != std::prev(cc.end()); ++it){    //std::prev to trim trailing space at EOL
+                std::cout << *it << " ";
+            }
+            std::cout << *(cc.rbegin()) << std::endl;
         }
     }
 
